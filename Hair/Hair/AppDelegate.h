b3143be0e02679b70a45f3d02f3ebf59503b900d
Tsuit <sui.t@163.com>
2015-08-25 22:31:32 +0800

    Create Hair

---
+++ Hair/Hair/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hair
+//
+//  Created by 任长平 on 15/8/25.
+//  Copyright (c) 2015年 任长平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hair/Hair/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hair
+//
+//  Created by 任长平 on 15/8/25.
+//  Copyright (c) 2015年 任长平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
